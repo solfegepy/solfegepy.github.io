c1f93a7fa12faedbd28f5bfe5d1c0d36895db5ba
seb <seb@email>
2026-08-07 17:24:06 +0100

    d

--- .devcontainer/_entrypoint.sh
+++ .devcontainer/_entrypoint.sh
@@ -4,13 +4,19 @@ set -e
 # Cleanup state from previous container run
 rm -f ui/.astro/dev.json
 
+# If using copilot
+~/.copilot/config_backup_restore_auth.py
+
 tmux new-session \; \
-  send-keys 'codex' \; \
-  split-window -v \; \
-  send-keys 'codex -p high' \; \
+  send-keys 'copilot' \; \
   split-window -v \; \
   send-keys 'm start-dev' C-m \; \
   select-pane -t 1
+  # split-window -v \; \
+  # send-keys 'm start-ui-dev' C-m \; \
+  # split-window -h \; \
+  # send-keys 'm start-api-dev' C-m \; \
+
 
 # In case we exit tmux
 # exec bash -l
