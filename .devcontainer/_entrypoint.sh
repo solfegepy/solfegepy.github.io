a255ed684d3dc0ab5056c05a955c2514c17d5538
seb <seb@email>
2026-09-07 00:08:51 +0100

    s

--- .devcontainer/_entrypoint.sh
+++ .devcontainer/_entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -e
 
+# Assert that .devcontainer generated files are still compatible with run-time version
+~/.local/bin/assert_saicli_version.sh "2026-09-06_23:51"
+
 # Cleanup state from previous container run
 rm -f ui/.astro/dev.json
 
 # If using copilot
 # ~/.copilot/config_backup_restore_auth.py
 
-herdr_spreader.py --file .devcontainer/herdr_start_config.yaml
+herdr_spreader.py --file .devcontainer/herdr_start_config.yaml --file ~/.local/share/herdr_common.yaml
 # user can only detach, so we stop herdr here
 herdr server stop
 
 # In case we exit tmux
-exec bash -l
+# exec bash -l
