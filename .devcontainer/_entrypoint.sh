64e45fd724f38b10901fac0a51171a1f33fb8b32
seb <seb@email>
2026-09-24 08:08:54 +0100

    d

--- .devcontainer/_entrypoint.sh
+++ .devcontainer/_entrypoint.sh
@@ -2,13 +2,10 @@
 set -e
 
 # Assert that .devcontainer generated files are still compatible with run-time version
-~/.local/bin/assert_saicli_version.sh "2026-09-06_23:51"
+~/.local/bin/assert_saicli_version.sh "2026-09-24_08:07"
 
 # Cleanup state from previous container run
 rm -f ui/.astro/dev.json
-
-# If using copilot
-# ~/.copilot/config_backup_restore_auth.py
 
 herdr_spreader.py --file .devcontainer/herdr_start_config.yaml --file ~/.local/share/herdr_common.yaml
 # user can only detach, so we stop herdr here
